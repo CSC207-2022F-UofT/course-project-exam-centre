0e50982959124cf79388a841e75d68a4274933df
harvey.donnelly <harveydonnelly404@gmail.com>
2022-11-16 10:12:00 -0500

    Changed userId attribute to String from int. Consolidated CommonUser/User interface class pair as one concrete User class.

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -1,7 +1,16 @@
 package entities;
 
-public interface UserFactory {
+public class UserFactory {
 
-    User create(String firstName, String lastName, String email, int userId);
-
+    /** Creates a new instance of User
+     *
+     * @param firstName First name of User
+     * @param lastName Last name of User
+     * @param email Email of User
+     * @param userId User's generated ID
+     * @return Returns a new instance of User
+     */
+    public User create(String firstName, String lastName, String email, String userId){
+        return new User(firstName, lastName, email, userId);
+    }
 }
