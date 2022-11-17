1d5e3293f6f91c1b98df4dd1b5424f7962523c68
nykoadionisio <50640686+nykoadionisio@users.noreply.github.com>
2022-11-17 18:29:40 -0500

    Added functions to the URegisterDsGateway
    Added ways to check if an account exists, save user information, and checking if a userId already exists.

--- src/main/java/uc/user/register/URegisterDsGateway.java
+++ src/main/java/uc/user/register/URegisterDsGateway.java
@@ -1,4 +1,9 @@
 package uc.user.register;
 
 public interface URegisterDsGateway {
+    boolean checkIfAccountExists(String email);
+
+    boolean saveUserInfo(URegisterDsRequestModel requestModel);
+    boolean checkIfIdExists(String userId);
+
 }
