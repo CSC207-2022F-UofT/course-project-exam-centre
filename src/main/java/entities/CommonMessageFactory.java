fb521543386a2b160f12d445a58c35e54cb7e461
N1cholasMcKee <105333582+N1cholasMcKee@users.noreply.github.com>
2022-11-21 13:00:07 -0500

    Update CommonMessageFactory.java
    Removed artifacts from merge conflict in CommonMessageFactory entity.

--- src/main/java/entities/CommonMessageFactory.java
+++ src/main/java/entities/CommonMessageFactory.java
@@ -2,12 +2,6 @@ package entities;
 
 // Entity layer
 
-<<<<<<< HEAD
-=======
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
->>>>>>> origin/8-use-case-8-submit-discussion-board-message
 public class CommonMessageFactory implements MessageFactory {
     @Override
     public Message create(int messageId, int solutionId, int userId, int parentId, String body) {
