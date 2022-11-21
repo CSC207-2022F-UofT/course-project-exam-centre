29d6034cd17fac9a9a599855ff1a99cf166126a2
N1cholasMcKee <105333582+N1cholasMcKee@users.noreply.github.com>
2022-11-21 12:59:22 -0500

    Update CommonMessage.java
    Removed artifacts from merge conflict.

--- src/main/java/entities/CommonMessage.java
+++ src/main/java/entities/CommonMessage.java
@@ -2,11 +2,6 @@ package entities;
 
 // Entity layer
 
-<<<<<<< HEAD
-=======
-import java.time.LocalDateTime;
-
->>>>>>> origin/8-use-case-8-submit-discussion-board-message
 class CommonMessage implements Message {
 
     private final int messageId;
@@ -18,11 +13,6 @@ class CommonMessage implements Message {
     private final int parentId;
     private final String body;
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/8-use-case-8-submit-discussion-board-message
     /** Creates a Message that contains the messageId, solutionId, parentId, body, timestamp
      *
      * @param messageId the unique id corresponding the Message.
