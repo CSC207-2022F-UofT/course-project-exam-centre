03a52faeaa0454a576d67a094cafa5bd4e6b5af0
harvey.donnelly <harveydonnelly404@gmail.com>
2022-11-26 19:29:27 -0500

    Added course info items tracking to state tracker entity

---
+++ src/main/java/entities/StateTrackerFactory.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class StateTrackerFactory {
+
+    /**
+     * Returns a new StateTracker object.
+     */
+    public StateTracker create () {
+        return new StateTracker();
+    }
+}
