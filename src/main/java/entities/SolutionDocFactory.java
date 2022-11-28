f940993068e5f26dd35c326cc552e468945d1048
harvey.donnelly <harveydonnelly404@gmail.com>
2022-11-27 19:55:07 -0500

    Updated SolutionDocument entity and factory

--- src/main/java/entities/SolutionDocFactory.java
+++ src/main/java/entities/SolutionDocFactory.java
@@ -2,20 +2,35 @@ package entities;
 
 public class SolutionDocFactory {
 
-
     /**
-     * Constructs a new Document abstract class for TestDoc or SolutionDoc to extend.
+     * Constructs a new SolutionDocument object.
      *
-     * @param name   The name of the document
-     * @param id     The document identifier
-     * @param course The course the document belongs to
-     * @param user   The user that uploaded the document
-     * @param score  The total score of the test
-     * @param testID The UofT id for the test
-     * @param rootID The rootID that hold all the messages for this solution.
-     * @param recordedTime the recorded time of the solution poster
+     * @param solutionName      The name of the document
+     * @param solutionId        The document identifier
+     * @param course            The course the document belongs to
+     * @param user              The user that uploaded the document
+     * @param score             The total score of the test
+     * @param parentTest        The parent TestDocument object
+     * @param rootMessageId     The root message id for this solution document
+     * @param recordedTime      The recorded time for the solution document
      */
-    public SolutionDocument create(String name, String id, Course course, User user, String score, String testID, Float recordedTime, String rootID) {
-        return new SolutionDocument(name, id, course, user, score, testID, recordedTime, rootID);
+    public SolutionDocument create(String solutionName,
+                                   String solutionId,
+                                   Course course,
+                                   User user,
+                                   Float score,
+                                   TestDocument parentTest,
+                                   Float recordedTime,
+                                   String rootMessageId) {
+
+        return new SolutionDocument(
+                solutionName,
+                solutionId,
+                course,
+                user,
+                score,
+                parentTest,
+                recordedTime,
+                rootMessageId);
     }
 }
