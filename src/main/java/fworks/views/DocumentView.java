126238cc17c378a2049a23179bae4978c10b9cbc
takedakento <107985058+takedakento@users.noreply.github.com>
2022-11-29 01:25:58 -0500

    Add constructors

--- src/main/java/fworks/views/DocumentView.java
+++ src/main/java/fworks/views/DocumentView.java
@@ -6,13 +6,26 @@ import org.icepdf.ri.util.*;
 import javax.swing.*;
 import java.util.prefs.Preferences;
 
+/**
+ * The view that renders and displays a PDF
+ */
 public class DocumentView {
     private String filePath;
     private SwingController controller;
     private SwingViewBuilder factory;
     private JPanel panel;
 
-    // Empty constructor for testing
+    /**
+     * @param filePath the file path of the PDF
+     */
+    public DocumentView(String filePath) {
+        this();
+        this.filePath = filePath;
+    }
+
+    /**
+     * Initialize with a default file path for testing
+     */
     public DocumentView() {
         filePath = "/Users/takedakento/Downloads/project/test/csc207h-d21.pdf";
         controller = new SwingController();
@@ -21,6 +34,9 @@ public class DocumentView {
         panel.setSize(700, 700);
     }
 
+    /**
+     * @return the panel component of the view
+     */
     public JPanel getPanel() {
         setPreferences();
         controller.openDocument(filePath);
