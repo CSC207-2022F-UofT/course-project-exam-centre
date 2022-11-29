5c89238a14543ca76da8fc0fd8e5ff775eaabcfa
takedakento <107985058+takedakento@users.noreply.github.com>
2022-11-29 01:06:23 -0500

    Add comments

--- src/main/java/driver/MainTest.java
+++ src/main/java/driver/MainTest.java
@@ -4,6 +4,9 @@ import fworks.views.SolutionFrame;
 import fworks.views.TestFrame;
 import fworks.views.WelcomeDialog;
 
+/**
+ * A driver for testing the UI
+ */
 public class MainTest {
     public static void main(String[] args) {
         new WelcomeDialog(); // Register/Log-in window
