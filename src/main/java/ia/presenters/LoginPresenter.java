9b7571f58dc92b69bf12f3b3c586e2f7c5e8aecf
harvey.donnelly <harveydonnelly404@gmail.com>
2022-12-02 16:48:40 -0500

    LoginPresenter updated

--- src/main/java/ia/presenters/LoginPresenter.java
+++ src/main/java/ia/presenters/LoginPresenter.java
@@ -6,13 +6,15 @@ import uc.user.login.LoginResponseModel;
 
 public class LoginPresenter implements LoginOutputBoundary {
     @Override
-    public LoginResponseModel prepareSuccessView(LoginResponseModel responseModel) {
+    public LoginResponseModel prepareSuccessView(
+            LoginResponseModel responseModel) {
+        // TODO: Update view model here
         return responseModel;
     }
 
     @Override
     public LoginResponseModel prepareFailView(String errorMessage) {
-        // Throw a custom exception
+        // TODO: Update view model here
         throw new LoginFailedException(errorMessage);
     }
 }
