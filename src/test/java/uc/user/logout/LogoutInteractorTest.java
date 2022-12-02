0bf9b9bf64ebb16fbbaa682164a6131344bda692
darlyn <darlynnguyen6697@gmail.com>
2022-12-01 23:01:38 -0500

    Wrote basic test for interactor.

---
+++ src/test/java/uc/user/logout/LogoutInteractorTest.java
@@ -0,0 +1,36 @@
+package uc.user.logout;
+
+import entities.StateTracker;
+import entities.User;
+import entities.UserFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogoutInteractorTest {
+    /**
+     * Test that logOut removes currentUser from the state tracker instance
+     */
+    @Test
+    public void logOutRemovesCurrentUser(){
+        LogoutOutputBoundary presenter = new LogoutOutputBoundary() {
+            @Override
+            public void prepareLoginView() {
+            }
+        };
+        StateTracker stateTracker = new StateTracker();
+        UserFactory userFactory = new UserFactory();
+        User user = userFactory.create("First",
+                                       "Last",
+                                       "FirstLast@mail.uoftears.ca",
+                                       "LOL123");
+
+        stateTracker.setCurrentUser(user);
+
+        // run the use case
+        LogoutInteractor interactor = new LogoutInteractor(presenter, stateTracker);
+        interactor.logOut();
+
+        assertNull(stateTracker.getCurrentUser());
+    }
+}
