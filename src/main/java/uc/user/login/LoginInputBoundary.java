5c4c6c4c09530cd7ef0e7711e3c2dcdac2d38aad
takedakento <107985058+takedakento@users.noreply.github.com>
2022-12-03 14:28:02 -0500

    Modify comments and fix code error

--- src/main/java/uc/user/login/LoginInputBoundary.java
+++ src/main/java/uc/user/login/LoginInputBoundary.java
@@ -2,12 +2,14 @@ package uc.user.login;
 
 /**
  * LoginInputBoundary provides an entry-point to the login use case.
- * Used by a Controller and implemented by LoginInteractor.
+ * Used by LoginController and implemented by LoginInteractor.
  * @layer use cases
  */
 public interface LoginInputBoundary {
-    /** Invoke login use case.
+    /**
+     * Invoke login use case.
      * @param requestModel
      * @return a LoginResponseModel corresponding to the requestModel
+     */
     LoginResponseModel logIn(LoginRequestModel requestModel);
 }
