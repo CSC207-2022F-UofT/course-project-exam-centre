807cfee40beacd2d756e5d4d7a08eb63a711640d
Nicholas Mckee <nmsmckee@gmail.com>
2022-12-02 21:13:16 -0500

    Added documentation for new controllers

--- src/main/java/ia/controllers/SubmitSolutionDocController.java
+++ src/main/java/ia/controllers/SubmitSolutionDocController.java
@@ -6,12 +6,30 @@ import uc.doc.submitsolution.SubmitSDocInputBoundary;
 
 public class SubmitSolutionDocController{
 
+    /**
+     * The input boundary class for the Submit document use case
+     */
     private final SubmitSDocInputBoundary inputBoundary;
 
+    /**
+     * Creates a new instance of SubmitSolutionDocController to take in data from the related screen
+     * @param inputBoundary The SubmitSDocInputBoundary from the Submit Document use case
+     */
     public SubmitSolutionDocController(SubmitSDocInputBoundary inputBoundary) {
         this.inputBoundary = inputBoundary;
     }
 
+    /**
+     * Takes in the information from the related screen, sends that information in an instance of
+     * SubmitSDocInputBoundary to the use case, and returns the related response model
+     * @param name Name of the document
+     * @param recordedScore The number of marks for the document
+     * @param courseID The ID of the related course
+     * @param recordedTime The time limit for the associated test
+     * @param parentTestID The ID of the corresponding test
+     * @param filePath The path to document on the user's computer
+     * @return A SubmitSDocResponseModel object
+     */
     public SubSDocResponseModel SubmitSolutionDoc(String name,
                                                   Float recordedScore,
                                                   String courseID,
