08f0a7ea66fde6007d41a74a48787d292948096a
darlyn <darlynnguyen6697@gmail.com>
2022-12-03 11:07:53 -0500

    Completed output boundary javadoc.

--- src/main/java/uc/user/login/LoginOutputBoundary.java
+++ src/main/java/uc/user/login/LoginOutputBoundary.java
@@ -2,11 +2,22 @@ package uc.user.login;
 
 /**
  * Methods defined in LoginOutputBoundary are used by LoginInteractor
- * to either change the view to the home screen,
- * or present a failure popup, which are implemented by a presenter.
+ * to either prepare the view to the home screen,
+ * or prepare a failure popup, which are implemented by a presenter.
  * @layer use cases
  */
 public interface LoginOutputBoundary {
+
+    /**
+     * Prepares the home screen.
+     * @param responseModel contains loginStatus and userId
+     */
     LoginResponseModel prepareSuccessView (LoginResponseModel responseModel);
+
+    /**
+     * Prepares a failure popup.
+     * @param errorMessage to be displayed to user
+     * @throws ia.presenters.LoginFailed exception
+     */
     LoginResponseModel prepareFailView (String errorMessage);
 }
