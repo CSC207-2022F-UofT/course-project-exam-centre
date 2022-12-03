47dc0cb5f2d6e1595fa099e586e038d33be633ac
darlyn <darlynnguyen6697@gmail.com>
2022-12-03 17:42:36 -0500

    Implemented new LoginDsResponseModel in login tests.

--- src/test/java/uc/user/login/LoginInteractorTest.java
+++ src/test/java/uc/user/login/LoginInteractorTest.java
@@ -15,26 +15,51 @@ public class LoginInteractorTest {
      *  given a correct password
      */
     @Test
-    void logInSuccess() {
+    public void logInSuccess() {
 
         LoginDsGateway dsGateway = new LoginDsGateway() {
+            // implement LoginDsGateway using anonymous class
+            private final String storedEmail = "firstlast@mail.uoftears.ca";
+            private final String storedPassword = ";-;";
+            private final String storedUserId = "ABCD1234";
+            private final String storedFirstName = "First";
+            private final String storedLastName = "Last";
             @Override
             public boolean verifyLoginCredentials(String email, String password) {
-                // compares to credentials in "DB"
-                return email.equals("firstname@mail.uoftears.ca")
-                        && password.equals(";-;");
+                return email.equals(storedEmail)
+                        && password.equals(storedPassword);
             }
 
             @Override
-            public LoginDsResponseModel getUserInfo(String email) {
-                return new LoginDsResponseModel("LOL123", "First", "Last");
+            public LoginDsResponseModel getUserByEmail(String email) {
+                // implement LoginDsResponseModel using anonymous class
+                return new LoginDsResponseModel() {
+                    @Override
+                    public String getUserId() {
+                        return storedUserId;
+                    }
+
+                    @Override
+                    public String getEmail() {
+                        return storedEmail;
+                    }
+
+                    @Override
+                    public String getFirstName() {
+                        return storedFirstName;
+                    }
+
+                    @Override
+                    public String getLastName() {
+                        return storedLastName;
+                    }
+                };
             }
         };
 
         LoginOutputBoundary presenter = new LoginOutputBoundary() {
             @Override
             public LoginResponseModel prepareSuccessView(LoginResponseModel responseModel) {
-                assertTrue(responseModel.isLoggedIn());
                 assertEquals("LOL123", responseModel.getUserId());
                 return null;
             }
@@ -50,15 +75,15 @@ public class LoginInteractorTest {
         StateTracker stateTracker = new StateTracker();
         LoginInteractor interactor = new LoginInteractor(stateTracker, dsGateway, presenter, userFactory);
 
-        // run the use case, supposing the user input is as follows
-        LoginRequestModel requestModel = new LoginRequestModel("firstname@mail.uoftears.ca", ";-;");
+        // run the use case, supposing the user input matches stored data
+        LoginRequestModel requestModel = new LoginRequestModel("firstlast@mail.uoftears.ca", ";-;");
         interactor.logIn(requestModel);
 
         // check if current user is tracked
         assertNotNull(stateTracker.getCurrentUser());
         User user = stateTracker.getCurrentUser();
-        assertEquals("LOL123", user.getId());
-        assertTrue(stateTracker.checkIfUserTracked("LOL123"));
+        assertEquals("ABCD1234", user.getId());
+        assertTrue(stateTracker.checkIfUserTracked("ABCD1234"));
     }
 
     /** Test that LoginInteractor prepares a fail view
@@ -66,19 +91,45 @@ public class LoginInteractorTest {
      *  given an incorrect password
      */
     @Test
-    void logInFail() {
+    public void logInFailGivenIncorrectPassword() {
 
         LoginDsGateway dsGateway = new LoginDsGateway() {
+            // implement LoginDsGateway using anonymous class
+            private final String storedEmail = "firstlast@mail.uoftears.ca";
+            private final String storedPassword = ";-;";
+            private final String storedUserId = "ABCD1234";
+            private final String storedFirstName = "First";
+            private final String storedLastName = "Last";
             @Override
             public boolean verifyLoginCredentials(String email, String password) {
-                // compares to credentials in "DB"
-                return email.equals("firstname@mail.uoftears.ca")
-                        && password.equals(";-;");
+                return email.equals(storedEmail)
+                        && password.equals(storedPassword);
             }
 
             @Override
-            public LoginDsResponseModel getUserInfo(String email) {
-                return new LoginDsResponseModel("LOL123", "First", "Last");
+            public LoginDsResponseModel getUserByEmail(String email) {
+                // implement LoginDsResponseModel using anonymous class
+                return new LoginDsResponseModel() {
+                    @Override
+                    public String getUserId() {
+                        return storedUserId;
+                    }
+
+                    @Override
+                    public String getEmail() {
+                        return storedEmail;
+                    }
+
+                    @Override
+                    public String getFirstName() {
+                        return storedFirstName;
+                    }
+
+                    @Override
+                    public String getLastName() {
+                        return storedLastName;
+                    }
+                };
             }
         };
 
@@ -100,12 +151,11 @@ public class LoginInteractorTest {
         LoginInteractor interactor = new LoginInteractor(stateTracker, dsGateway, presenter, userFactory);
 
         // run the use case, supposing the user input is as follows
-        LoginRequestModel requestModel = new LoginRequestModel("firstname@mail.uoftears.ca", ":(");
+        LoginRequestModel requestModel = new LoginRequestModel("firstlast@mail.uoftears.ca", ":(");
         interactor.logIn(requestModel);
 
         // check if current user is tracked
         assertNull(stateTracker.getCurrentUser());
-        assertFalse(stateTracker.checkIfUserTracked("LOL123"));
+        assertFalse(stateTracker.checkIfUserTracked("ABCD1234"));
     }
-
 }
