f4d24747813a357d4c218fa56de7f612290396b8
takedakento <107985058+takedakento@users.noreply.github.com>
2022-12-03 14:53:07 -0500

    Add and modify comments

--- src/main/java/uc/user/login/LoginOutputBoundary.java
+++ src/main/java/uc/user/login/LoginOutputBoundary.java
@@ -1,22 +1,22 @@
 package uc.user.login;
 
 /**
- * LoginOutputBoundary provides methods for the LoginInteractor.
- * They should be implemented by a presenter.
+ * LoginOutputBoundary provides methods for LoginInteractor.
+ * They should be implemented by LoginPresenter.
  * @layer use cases
  */
 public interface LoginOutputBoundary {
-
     /**
      * Prepares the home screen.
      * @param responseModel contains loginStatus and userId
+     * @return a ResponseModel corresponding to successful log in
      */
-    LoginResponseModel prepareSuccessView (LoginResponseModel responseModel);
+    LoginResponseModel prepareSuccessView(LoginResponseModel responseModel);
 
     /**
      * Prepares a failure popup.
      * @param errorMessage to be displayed to user
      * @throws ia.presenters.LoginFailed runtime exception
      */
-    LoginResponseModel prepareFailView (String errorMessage);
+    LoginResponseModel prepareFailView(String errorMessage);
 }
