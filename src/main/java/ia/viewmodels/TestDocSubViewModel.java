f1755dd343f210fa7db048aa27edff74ab7e1719
harvey.donnelly <harveydonnelly404@gmail.com>
2022-12-07 17:15:41 -0500

    Implement main view model

---
+++ src/main/java/ia/viewmodels/TestDocSubViewModel.java
@@ -0,0 +1,61 @@
+package ia.viewmodels;
+
+import java.util.Map;
+
+public class TestDocSubViewModel {
+
+    private final String testId;
+    private final String userId;
+    private final String testType;
+    private final Integer numOfQuestions;
+    private final Float estimatedTime;
+    private final String testName;
+    private final Map<String, SolutionDocSubViewModel> solutionModels;
+
+    public TestDocSubViewModel(
+            String testId,
+            String userId,
+            String testType,
+            Integer numOfQuestions,
+            Float estimatedTime,
+            String testName,
+            Map<String, SolutionDocSubViewModel> solutionModels) {
+
+        this.testId = testId;
+        this.userId = userId;
+        this.testType = testType;
+        this.numOfQuestions = numOfQuestions;
+        this.estimatedTime = estimatedTime;
+        this.testName = testName;
+        this.solutionModels = solutionModels;
+    }
+
+    public Map<String, SolutionDocSubViewModel> getSolutionModels() {
+        return this.solutionModels;
+    }
+
+    public String getTestId() {
+        return this.testId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public String getTestType() {
+        return this.testType;
+    }
+
+    public Integer getNumOfQuestions() {
+        return this.numOfQuestions;
+    }
+
+    public Float getEstimatedTime() {
+        return this.estimatedTime;
+    }
+
+    public String getTestName() {
+        return this.testName;
+    }
+
+}
