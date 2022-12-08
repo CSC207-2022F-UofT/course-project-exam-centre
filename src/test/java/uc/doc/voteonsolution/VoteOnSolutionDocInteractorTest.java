f9ad6cf867705aaf2a037594cbdeb6b73b8115ba
darlyn <darlynnguyen6697@gmail.com>
2022-12-08 00:45:20 -0500

    Implemented VoteOnSolutionDocInteractor unit test cases.

--- src/test/java/uc/doc/voteonsolution/VoteOnSolutionDocInteractorTest.java
+++ src/test/java/uc/doc/voteonsolution/VoteOnSolutionDocInteractorTest.java
@@ -6,15 +6,17 @@ import entities.factories.SolutionDocFactory;
 import entities.factories.TestDocFactory;
 import entities.factories.UserFactory;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class VoteOnSolutionDocInteractorTest {
-
-    private SolutionDocument solution1;
-    private SolutionDocument solution2;
+    private User user;
+    private Course course;
+    private TestDocument testDoc;
+    private SolutionDocument solutionDoc;
     private StateTracker stateTracker;
-
+    private int storedVoteTotal;
     @Before
     public void setup(){
         SolutionDocFactory solutionDocFactory = new SolutionDocFactory();
@@ -22,44 +24,61 @@ public class VoteOnSolutionDocInteractorTest {
         UserFactory userFactory = new UserFactory();
         TestDocFactory testDocFactory = new TestDocFactory();
 
-        User user = userFactory.create("First",
-                                        "Last",
-                                        "firstlast@mail.uoftears.ca",
-                                        "userId");
+        user = userFactory.create("First", "Last", "firstlast@mail.uoftears.ca", "userId");
+        course = courseFactory.create("Software Design","CSC207", "courseId");
+        testDoc = testDocFactory.create("Final Exam #1", "testId", course, user, 0.0F, 1, "Final");
+        solutionDoc = solutionDocFactory.create("Solution", "solutionId", course, user, 0.0F, testDoc, 0.0F, "rootMsgId");
 
-        Course course = courseFactory.create("Software Design",
-                                            "CSC207",
-                                            "courseId");
+        course.addTest(testDoc);
+        testDoc.addUpdateSolution(solutionDoc);
 
-        TestDocument testDoc = testDocFactory.create("Final Exam #1", "testid", course, user, 0.0F, 1, "Final");
-
-        solution1 = solutionDocFactory.create("Solution #1", "solution1Id", course, user, 0.0F, testDoc, 0.0F, "rootMsgId");
-        solution2 = solutionDocFactory.create("Solution #2", "solution2Id", course, user, 0.0F, testDoc, 0.0F, "rootMsgId");
         stateTracker = new StateTracker();
         stateTracker.setCurrentUser(user);
+        stateTracker.addUpdateTrackedCourse(course);
+
+        storedVoteTotal = 0;
     }
 
-    public void upvoteIncrementsVoteTotal() {
+    /**
+     * Test that voteSolutionDoc increments solution document vote total when vote is true.
+     */
+    @Test
+    public void voteSolutionDocIncrementsVoteTotal() {
 
         VoteSDocDsGateway dsGateway = new VoteSDocDsGateway() {
             @Override
             public int getVoteTotalBySolutionIdQuery(String solutionId) {
-                return 0;
+                return storedVoteTotal;
             }
 
             @Override
             public boolean updateSolutionDocVote(VoteSDocDsRequestModel model) {
-                assertEquals(1, model.getVote());
-                assertEquals("solution1Id", model.getSolutionId());
-                assertEquals("userId", model.getUserId());
+                // check if persistence is correctly updated
+                assertEquals(storedVoteTotal + 1, model.getVote());
+                assertEquals(solutionDoc.getId(), model.getSolutionId());
+                assertEquals(user.getId(), model.getUserId());
                 return true;
+            }
+
+            @Override
+            public String getTestIdBySolutionId(String solutionId) {
+                return testDoc.getId();
+            }
+
+            @Override
+            public String getCourseIdByTestId(String testId) {
+                return course.getId();
             }
         };
 
         VoteSDocOutputBoundary presenter = new VoteSDocOutputBoundary() {
             @Override
             public VoteSDocResponseModel prepareSuccessView(VoteSDocResponseModel model) {
-                assertEquals(1, model.getVoteTotal());
+                // check if output data is correct
+                assertEquals(course.getId(), model.getCourseId());
+                assertEquals(testDoc.getId(), model.getTestId());
+                assertEquals(solutionDoc.getId(), model.getSolutionId());
+                assertEquals(storedVoteTotal + 1, model.getVoteTotal());
                 return null;
             }
 
@@ -69,12 +88,64 @@ public class VoteOnSolutionDocInteractorTest {
                 return null;
             }
         };
-        VoteOnSolutionDocInteractor interactor = new VoteOnSolutionDocInteractor(dsGateway, presenter, stateTracker);
 
-        // TODO: Create response models
+        VoteSDocRequestModel requestModel = new VoteSDocRequestModel(solutionDoc.getId(), true);
+        VoteOnSolutionDocInteractor interactor = new VoteOnSolutionDocInteractor(dsGateway, presenter, stateTracker);
+        interactor.voteSolutionDoc(requestModel);
     }
 
-    public void downvoteDecrementsVoteTotal() {
-        // TODO
+
+    /**
+     * Test that voteSolutionDoc increments solution document vote total when vote is false.
+     */
+    @Test
+    public void voteSolutionDocDecrementsVoteTotal() {
+        VoteSDocDsGateway dsGateway = new VoteSDocDsGateway() {
+            @Override
+            public int getVoteTotalBySolutionIdQuery(String solutionId) {
+                return storedVoteTotal;
+            }
+
+            @Override
+            public boolean updateSolutionDocVote(VoteSDocDsRequestModel model) {
+                // check if persistence is correctly updated
+                assertEquals(storedVoteTotal - 1, model.getVote());
+                assertEquals(solutionDoc.getId(), model.getSolutionId());
+                assertEquals(user.getId(), model.getUserId());
+                return true;
+            }
+
+            @Override
+            public String getTestIdBySolutionId(String solutionId) {
+                return testDoc.getId();
+            }
+
+            @Override
+            public String getCourseIdByTestId(String testId) {
+                return course.getId();
+            }
+        };
+
+        VoteSDocOutputBoundary presenter = new VoteSDocOutputBoundary() {
+            @Override
+            public VoteSDocResponseModel prepareSuccessView(VoteSDocResponseModel model) {
+                // check if output data is correct
+                assertEquals(course.getId(), model.getCourseId());
+                assertEquals(testDoc.getId(), model.getTestId());
+                assertEquals(solutionDoc.getId(), model.getSolutionId());
+                assertEquals(storedVoteTotal - 1, model.getVoteTotal());
+                return null;
+            }
+
+            @Override
+            public VoteSDocDsRequestModel prepareFailureView(String errorMessage) {
+                fail("Use case failure is unexpected.");
+                return null;
+            }
+        };
+
+        VoteSDocRequestModel requestModel = new VoteSDocRequestModel(solutionDoc.getId(), false);
+        VoteOnSolutionDocInteractor interactor = new VoteOnSolutionDocInteractor(dsGateway, presenter, stateTracker);
+        interactor.voteSolutionDoc(requestModel);
     }
 }
