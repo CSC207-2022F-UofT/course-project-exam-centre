82c656cebeee40458a64033f80737ddec4a9c6dc
nykoadionisio <nykoad04@gmail.com>
2022-12-08 12:49:10 -0500

    Update SolutionToolbar.java
    Update the dropdown to update based on what files are in the main view model and download files that arent on the local machine.

--- src/main/java/fworks/views/SolutionToolbar.java
+++ src/main/java/fworks/views/SolutionToolbar.java
@@ -1,5 +1,6 @@
 package fworks.views;
 
+import ia.controllers.DownloadDocController;
 import ia.controllers.SubmitSolutionDocController;
 import ia.viewmodels.CourseSubViewModel;
 import ia.viewmodels.MainViewModel;
@@ -8,8 +9,8 @@ import ia.viewmodels.SolutionDocSubViewModel;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * The panel component for SolutionFrame
@@ -17,8 +18,8 @@ import java.util.Map;
  */
 public class SolutionToolbar extends JPanel implements ActionListener {
     DocumentView docView;
+    DownloadDocController downloadDocController;
     private JButton discussionButton;
-
     private JButton uploadSolutionsButton;
     private JComboBox<String> chooseSolutionDoc;
     private SubmitSolutionDocController submitSolutionDocController;
@@ -28,34 +29,24 @@ public class SolutionToolbar extends JPanel implements ActionListener {
      *
      * @param docView
      */
-    public SolutionToolbar(DocumentView docView, MainViewModel mvm, SubmitSolutionDocController ssdc) {
+    public SolutionToolbar(DocumentView docView, MainViewModel mvm, SubmitSolutionDocController ssdc,
+                           DownloadDocController downloadDocController) {
         this.docView = docView;
         this.submitSolutionDocController = ssdc;
+        this.downloadDocController = downloadDocController;
         this.mainViewModel = mvm;
 
-        List solutionList = new List();
 
-        // TODO: Replace the solution list with a list of actual solutions available for the test
-        // Get the current data
-        String currCourse = mainViewModel.getCurrentCourseId();
-        String currTestId = mainViewModel.getCurrentTestId();
-        String currSolutionId = mainViewModel.getCurrentSolutionId();
+        Set<String> solutionSet = getSolutionMap().keySet();
+        // Getting an array of all the solutionIds for the ComboBox
+        String[] solutionList = (String[]) solutionSet.toArray();
 
-        Map<String, CourseSubViewModel> courses = mainViewModel.getCurrentUserCourseModels();
-        Map<String, SolutionDocSubViewModel> solutions =
-                courses.get(currCourse).getTests().get(currTestId).getSolutionModels();
-
-        for(SolutionDocSubViewModel i: solutions.values() ){
-            solutionList.add(i.getSolutionName());
-
-        //Update current test/solution first
-            // First we get all the names, if click occurs check if its downloaded and if not download it.
-            // If no solutions hide the view
-        }
-
+        chooseSolutionDoc = new JComboBox<>(solutionList);
         discussionButton = new JButton("Discussions");
         uploadSolutionsButton = new JButton("Upload solution");
-        chooseSolutionDoc = new JComboBox<String>(solutionList);
+
+
+
 //        docView.setFilePath(solutionList[0]);
 
         discussionButton.addActionListener(this);
@@ -70,6 +61,21 @@ public class SolutionToolbar extends JPanel implements ActionListener {
 
     }
 
+    /**
+     * Gets the map of the SolutionId and SolutionDocSubViewModel from the main view model
+     * @return a mapping of the SolutionId to its subviewmodel
+     */
+    private Map<String, SolutionDocSubViewModel> getSolutionMap(){
+        // Get the current data
+        String currCourse = mainViewModel.getCurrentCourseId();
+        String currTestId = mainViewModel.getCurrentTestId();
+
+        // Getting the solutionIds
+        Map<String, CourseSubViewModel> courses = mainViewModel.getCurrentUserCourseModels();
+
+        return courses.get(currCourse).getTests().get(currTestId).getSolutionModels();
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals(discussionButton.getText())) {
@@ -81,9 +87,28 @@ public class SolutionToolbar extends JPanel implements ActionListener {
                     mainViewModel.getCurrentTestId());
         }else if (actionEvent.getSource().equals(chooseSolutionDoc)){
             JComboBox action = (JComboBox)actionEvent.getSource();
-            String solutionName = (String) action.getSelectedItem();
-            docView.setFilePath(solutionName);
-            docView.loadFile();
+            String solutionId = action.getSelectedItem().toString();
+            // Check if the Solution is downloaded, if it is, load the filepath
+            if(mainViewModel.checkIfLocalDocumentPathExists(solutionId)){
+                String filepath = mainViewModel.getLocalDocumentPath(solutionId);
+                docView.setFilePath(filepath);
+                docView.loadFile();
+            // If not downloaded, try download the file and if not throw the error
+            } else {
+                downloadDocController.createInput(solutionId,
+                            mainViewModel.getCurrentUserModel().getUserId());
+
+                if(mainViewModel.checkIfLocalDocumentPathExists(solutionId)) {
+                    String filepath = mainViewModel.getLocalDocumentPath(solutionId);
+                    docView.setFilePath(filepath);
+                    docView.loadFile();
+                } else{
+                    JOptionPane.showMessageDialog(this,
+                            String.format("Unable to load %s", solutionId));
+                }
+
+
+            }
         }
     }
 }
