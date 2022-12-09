4c79dfa38bc0c96d129db7681c8c91a8826746fb
Lim Zhi Hao <70679812+pandaponn@users.noreply.github.com>
2022-12-08 22:18:29 -0500

    added javadoc fileaccessgateway

--- src/main/java/ia/gateways/FileAccessGateway.java
+++ src/main/java/ia/gateways/FileAccessGateway.java
@@ -9,25 +9,57 @@ import uc.doc.submittest.SubmitTDocFileAccessGateway;
 
 public interface FileAccessGateway extends SubmitSDocFileAccessGateway, SubmitTDocFileAccessGateway, DownloadDocFileAccessGateway {
     
+    /** Upload a file to the FTP server
+     *
+     * @param localFilePath         file path of local file 
+     * @param fileName              file name
+     * @return returns true if file upload is successful, false otherwise
+     */
     boolean uploadFile(String filePath, String docId);
 
+    /** Upload a solution document to the FTP server
+     * 
+     * @param model         request model containing information on the document to be uploaded
+     * @param docId         Id of document to be uploaded
+     * @return returns true if file upload is successful, false otherwise
+     */
     default boolean uploadSolutionDocument(SubmitSDocDsRequestModel model, String docId) {
         String filePath = model.getFilePath();
         return uploadFile(filePath, docId);
     }
 
+    /** Upload a test document to the FTP server
+     * 
+     * @param dsRequestModel    request model containing information on the document to be uploaded
+     * @param docId             Id of document to be uploaded
+     * @return returns true if file upload is successful, false otherwise
+     */
     default boolean uploadTestDocument(SubmitTDocDsRequestModel dsRequestModel, String docId) {
         String filePath = dsRequestModel.getFilePath();
         return uploadFile(filePath, docId);
     }
 
+    /** Download a file from FTP server
+     *
+     * @param fileName          file name to download
+     * @return returns the local file path if file is downloaded successfully, null otherwise
+     */
     String downloadFile(String fileName);
 
+    /** Download a file from FTP server
+     *
+     * @param model             request model containing information on the document to be downloaded
+     * @return returns the local file path if file is downloaded successfully, null otherwise
+     */
     default String downloadDoc(DownloadDocFileAccessRequestModel model) {
         String documentId = model.getDocumentId();
         return downloadFile(documentId);
     }
     
+    /** Check connection status to FTP server
+     * 
+     * @return true if connection is successful, false otherwise
+     */
     boolean checkConnectionStatus();
 
 }
