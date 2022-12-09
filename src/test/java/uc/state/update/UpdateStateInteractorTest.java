67321e5b270d998a2540b746c1069ae34b567530
darlyn <darlynnguyen6697@gmail.com>
2022-12-08 22:02:39 -0500

    Wrote test case for state tracker.

--- src/test/java/uc/state/update/UpdateStateInteractorTest.java
+++ src/test/java/uc/state/update/UpdateStateInteractorTest.java
@@ -1,23 +1,49 @@
 package uc.state.update;
 
+import entities.Course;
+import entities.StateTracker;
+import entities.factories.*;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import uc.state.update.dbmodels.*;
-
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class UpdateStateInteractorTest {
     private static UpdateStateTestData testData;
+    private static UserFactory userFactory;
+    private static CourseFactory courseFactory;
+    private static TestDocFactory testFactory;
+    private static SolutionDocFactory solutionFactory;
+    private static MessageFactory messageFactory;
+    private StateTracker currentState;
 
     @BeforeClass
     public static void setupBeforeClass(){
         testData = new UpdateStateTestData();
+
+        userFactory = new UserFactory();
+        courseFactory = new CourseFactory();
+        testFactory = new TestDocFactory();
+        solutionFactory = new SolutionDocFactory();
+        messageFactory = new MessageFactory();
     }
 
-    public void updateStateSuccessGivenUserNull(){
+    @Before
+    public void setup(){
+        currentState = new StateTracker();
+    }
+    /**
+     * Test that update state successfully reloads all courses tracked in state
+     * Given successful connection and null user
+     */
+    @Test
+    public void updateStateReloadsAllCoursesNullUser(){
         UpdateStateDsGateway dsGateway = new UpdateStateDsGateway() {
+            final boolean connectionStatus = true;
             @Override
             public List<? extends UpdateStateTestDocDbModel> getTestDocsByCourseId(String courseId) {
                 return null;
@@ -30,39 +56,42 @@ public class UpdateStateInteractorTest {
 
             @Override
             public List<? extends UpdateStateMessageDbModel> getMessagesByParentId(String parentId) {
-                return null;
+                return testData.getStoredMessagesByParentId(parentId);
             }
 
             @Override
             public UpdateStateUserDbModel getUserById(String userId) {
-                return null;
+                return testData.getUserDbResponseModelById(userId);
             }
 
             @Override
             public UpdateStateCourseDbModel getCourseById(String courseId) {
-                return null;
+                return testData.getCourseDbResponseModelById(courseId);
             }
 
             @Override
             public List<String> getAllCourseIds() {
-                return null;
+                return testData.getStoredAllCourseIds();
             }
 
             @Override
             public List<String> getCourseIdsByUserId(String userId) {
-                return null;
+                return testData.getStoredCourseIdsByUserId(userId);
             }
 
             @Override
             public boolean getConnectionStatus() {
-                return false;
+                return connectionStatus;
             }
         };
 
         UpdateStateOutputBoundary presenter = new UpdateStateOutputBoundary() {
             @Override
             public UpdateStateResponseModel prepareSuccessView(UpdateStateResponseModel responseModel) {
-                // TODO: check responseModel
+                // check that the presenter does not display any changes
+                assertNull(responseModel.getCurrentUserModel());
+                assertTrue(responseModel.getCourseInfoModels().isEmpty());
+                assertTrue(responseModel.getCurrentUserCourseModels().isEmpty());
                 return null;
             }
 
@@ -72,24 +101,29 @@ public class UpdateStateInteractorTest {
                 return null;
             }
         };
-
-        UpdateStateInteractor interactor = new UpdateStateInteractor();
+        UpdateStateInteractor interactor = new UpdateStateInteractor(presenter,
+                                                                     dsGateway,
+                                                                     currentState,
+                                                                     userFactory,
+                                                                     courseFactory,
+                                                                     testFactory,
+                                                                     solutionFactory,
+                                                                     messageFactory);
+        interactor.updateState();
+        Map<String, Course> trackedCourses = currentState.getAllTrackedCourses();
+        List<String> storedAllCourseIds = testData.getStoredAllCourseIds();
+        assertEquals(storedAllCourseIds.size(), trackedCourses.size());
     }
 
-    @Test
-    public void updateStateSuccessGivenUserNotNull(){
-
-        UpdateStateInteractor interactor = new UpdateStateInteractor();
-    }
-
-    @Test
-    public void updateStateSuccessGivenDbConnectionSuccess(){
-
-        UpdateStateInteractor interactor = new UpdateStateInteractor();
-    }
-
-    @Test
-    public void updateStateFailGivenDbConnectionFail(){
-        UpdateStateInteractor interactor = new UpdateStateInteractor();
-    }
+//    @Test
+//    public void updateStateSuccessGivenUserNotNullConnectionSuccess(){
+//
+//        UpdateStateInteractor interactor = new UpdateStateInteractor();
+//    }
+//
+//
+//    @Test
+//    public void updateStateFailGivenDbConnectionFail(){
+//        UpdateStateInteractor interactor = new UpdateStateInteractor();
+//    }
 }
--- src/test/java/uc/state/update/UpdateStateTestData.java
+++ src/test/java/uc/state/update/UpdateStateTestData.java
@@ -1,175 +1,326 @@
 package uc.state.update;
 
-import entities.factories.*;
 import uc.state.update.dbmodels.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UpdateStateTestData {
-    private static UserFactory userFactory;
-    private static CourseFactory courseFactory;
-    private static TestDocFactory testFactory;
-    private static SolutionDocFactory solutionFactory;
-    private static MessageFactory messageFactory;
-    private static UpdateStateUserDbModel userDbResponseModel;
-    private static UpdateStateCourseDbModel courseDbResponseModel;
-    private static UpdateStateTestDocDbModel testDbResponseModel;
-    private static UpdateStateSolutionDocDbModel solutionDbResponseModel;
-    private static UpdateStateMessageDbModel messageDbResponseModel;
-    private static List<String> storedAllCourseIds;
-    private static List<String> storedUsersCourseIds;
+    private Map<String, UpdateStateUserDbModel> storedUsers; // retrieved via userId
+    private Map<String, UpdateStateCourseDbModel> storedCourses; // retrieved via courseId
+    private Map<String, List<UpdateStateTestDocDbModel>> storedTests; // retrieved via courseId
+    private Map<String, List<UpdateStateSolutionDocDbModel>> storedSolutions; // retrieved via testId
+    private Map<String, List<UpdateStateMessageDbModel>> storedMessages; // retrieved via parentId
+    private List<String> storedAllCourseIds;
+    private Map<String, List<String>> storedCourseIdsByUserId;
 
     public UpdateStateTestData() {
-        userFactory = new UserFactory();
-        courseFactory = new CourseFactory();
-        testFactory =  new TestDocFactory();
-        solutionFactory = new SolutionDocFactory();
-        messageFactory = new MessageFactory();
+        createStoredUsers();
+        createStoredCourses();
+        createStoredTests();
+        createStoredSolutions();
+        createdStoredMessages();
+        createStoredAllCourseIds();
+        createStoredCourseIdsByUserId();
+    }
 
-        userDbResponseModel = new UpdateStateUserDbModel() {
+    private void createStoredUsers() {
+        storedUsers = new HashMap<>();
+        UpdateStateUserDbModel storedUser1 = new UpdateStateUserDbModel() {
             @Override
             public String getUserId() {
-                return null;
+                return "user1Id";
             }
 
             @Override
             public String getFirstName() {
-                return null;
+                return "User";
             }
 
             @Override
             public String getLastName() {
-                return null;
+                return "One";
             }
 
             @Override
             public String getEmail() {
-                return null;
+                return "user.one@mail.utoronto.ca";
             }
         };
 
-        courseDbResponseModel = new UpdateStateCourseDbModel() {
+        UpdateStateUserDbModel storedUser2 = new UpdateStateUserDbModel() {
+            @Override
+            public String getUserId() {
+                return "user2Id";
+            }
+
+            @Override
+            public String getFirstName() {
+                return "User";
+            }
+
+            @Override
+            public String getLastName() {
+                return "Two";
+            }
+
+            @Override
+            public String getEmail() {
+                return "user.two@mail.utoronto.ca";
+            }
+        };
+    storedUsers.put(storedUser1.getUserId(), storedUser1);
+    storedUsers.put(storedUser2.getUserId(), storedUser2);
+    }
+
+    private void createStoredCourses() {
+        storedCourses = new HashMap<>();
+        UpdateStateCourseDbModel csc207 = new UpdateStateCourseDbModel() {
             @Override
             public String getCourseId() {
-                return null;
+                return "csc207Id";
             }
 
             @Override
             public String getCourseCode() {
-                return null;
+                return "CSC207";
             }
 
             @Override
             public String getCourseName() {
-                return null;
+                return "Software Design";
             }
         };
+        UpdateStateCourseDbModel mat137 = new UpdateStateCourseDbModel() {
+            @Override
+            public String getCourseId() {
+                return "mat137Id";
+            }
 
-        testDbResponseModel = new UpdateStateTestDocDbModel() {
+            @Override
+            public String getCourseCode() {
+                return "MAT137";
+            }
+
+            @Override
+            public String getCourseName() {
+                return "Calculus with Anxiety";
+            }
+        };
+        storedCourses.put(csc207.getCourseId(), csc207);
+        storedCourses.put(mat137.getCourseId(), mat137);
+    }
+
+    private void createStoredTests() {
+        storedTests = new HashMap<>();
+
+        List<UpdateStateTestDocDbModel> csc207Tests = new ArrayList<>();
+        UpdateStateTestDocDbModel csc207Test = new UpdateStateTestDocDbModel() {
             @Override
             public String getTestId() {
-                return null;
+                return "csc207TestId";
             }
 
             @Override
             public String getUserId() {
-                return null;
+                return "";
             }
 
             @Override
             public String getTestType() {
-                return null;
+                return "";
             }
 
             @Override
             public Integer getNumOfQuestions() {
-                return null;
+                return 0;
             }
 
             @Override
             public Float getEstimatedTime() {
-                return null;
+                return 0.0F;
             }
 
             @Override
             public String getTestName() {
-                return null;
+                return "";
             }
         };
 
-        solutionDbResponseModel = new UpdateStateSolutionDocDbModel() {
-            @Override
-            public String getSolutionId() {
-                return null;
-            }
+        csc207Tests.add(csc207Test);
+        storedTests.put("csc207Id", csc207Tests);
 
-            @Override
-            public String getSolutionName() {
-                return null;
-            }
-
+        List<UpdateStateTestDocDbModel> mat137Tests = new ArrayList<>();
+        UpdateStateTestDocDbModel mat137Test = new UpdateStateTestDocDbModel() {
             @Override
             public String getTestId() {
-                return null;
+                return "mat137TestId";
             }
 
             @Override
             public String getUserId() {
-                return null;
+                return "";
             }
 
             @Override
-            public Integer getVoteTotal() {
-                return null;
+            public String getTestType() {
+                return "";
             }
 
             @Override
-            public Float getRecordedScore() {
-                return null;
+            public Integer getNumOfQuestions() {
+                return 0;
             }
 
             @Override
             public Float getEstimatedTime() {
-                return null;
+                return 0.0F;
             }
 
             @Override
-            public String getRootMessageId() {
-                return null;
+            public String getTestName() {
+                return "";
             }
         };
+        mat137Tests.add(mat137Test);
+        storedTests.put("mat137Id", csc207Tests);
+    }
 
-        messageDbResponseModel = new UpdateStateMessageDbModel() {
+    private void createStoredSolutions() {
+        storedSolutions = new HashMap<>();
+        List<UpdateStateSolutionDocDbModel> csc207TestSolutions = new ArrayList<>();
+        UpdateStateSolutionDocDbModel csc207TestSolution = new UpdateStateSolutionDocDbModel() {
             @Override
-            public String getMessageId() {
-                return null;
+            public String getSolutionId() {
+                return "csc207SolutionTestId";
             }
 
             @Override
-            public String getSolutionId() {
-                return null;
+            public String getSolutionName() {
+                return "";
+            }
+
+            @Override
+            public String getTestId() {
+                return "csc207TestId";
             }
 
             @Override
             public String getUserId() {
-                return null;
+                return "";
+            }
+
+            @Override
+            public Integer getVoteTotal() {
+                return 0;
+            }
+
+            @Override
+            public Float getRecordedScore() {
+                return 0.0F;
+            }
+
+            @Override
+            public Float getEstimatedTime() {
+                return 0.0F;
+            }
+
+            @Override
+            public String getRootMessageId() {
+                return "";
+            }
+        };
+        csc207TestSolutions.add(csc207TestSolution);
+        storedSolutions.put(csc207TestSolution.getTestId(), csc207TestSolutions);
+    }
+
+    private void createdStoredMessages() {
+        storedMessages = new HashMap<>();
+        List<UpdateStateMessageDbModel> csc207SolutionMessages = new ArrayList<>();
+
+        UpdateStateMessageDbModel csc207SolutionMessage = new UpdateStateMessageDbModel() {
+            @Override
+            public String getMessageId() {
+                return "";
+            }
+
+            @Override
+            public String getSolutionId() {
+                return "";
+            }
+
+            @Override
+            public String getUserId() {
+                return "";
             }
 
             @Override
             public String getParentId() {
-                return null;
+                return "parentId";
             }
 
             @Override
             public String getMessageBody() {
-                return null;
+                return "csc207 rocks!";
             }
 
             @Override
             public LocalDateTime getMessageSentTimestamp() {
-                return null;
+                return LocalDateTime.now();
             }
         };
+        csc207SolutionMessages.add(csc207SolutionMessage);
+        storedMessages.put(csc207SolutionMessage.getSolutionId(), csc207SolutionMessages);
+    }
+
+    private void createStoredAllCourseIds() {
+        storedAllCourseIds = new ArrayList<>();
+        storedAllCourseIds.add("csc207Id");
+        storedAllCourseIds.add("mat207Id");
+    }
+
+    private void createStoredCourseIdsByUserId(){
+        storedCourseIdsByUserId = new HashMap<>();
+        // user 1 is enrolled in csc207
+        // user 2 is enrolled in mat137
+        List<String> user1Courses = new ArrayList<>();
+        user1Courses.add("csc207Id");
+
+        List<String> user2Courses = new ArrayList<>();
+        user2Courses.add("mat137Id");
+
+        storedCourseIdsByUserId.put("user1Id", user1Courses);
+        storedCourseIdsByUserId.put("user2Id", user2Courses);
+    }
+
+    public UpdateStateUserDbModel getUserDbResponseModelById(String userId) {
+        return storedUsers.get(userId);
+    }
+
+    public UpdateStateCourseDbModel getCourseDbResponseModelById(String courseId) {
+        return storedCourses.get(courseId);
+    }
+
+    public List<UpdateStateTestDocDbModel> getStoredTestsByCourseId(String courseId) {
+        return storedTests.get(courseId);
+    }
+
+    public List<UpdateStateSolutionDocDbModel> getStoredSolutionsByTestId(String testId) {
+        return storedSolutions.get(testId);
+    }
+
+    public List<UpdateStateMessageDbModel> getStoredMessagesByParentId(String parentId) {
+        return storedMessages.get(parentId);
+    }
+
+    public List<String> getStoredAllCourseIds(){
+        return storedAllCourseIds;
+    }
+
+    public List<String> getStoredCourseIdsByUserId(String userId){
+        return storedCourseIdsByUserId.get(userId);
     }
 }
